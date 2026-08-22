b062c422168c07044ee39a3c8e9f0718e6e505d2
花小孩 <14057717+flower-child@user.noreply.gitee.com>
2026-08-22 11:54:37 +0800

    个性化配置主题

---
+++ update.sh
@@ -0,0 +1,10 @@
+# 如果没有消息后缀，默认提交信息为 `:pencil: update content`
+info=$1
+if ["$info" = ""];
+then info=":pencil: update content"
+fi
+git add -A
+git commit -m "$info"
+git push origin hexo
+
+# 此后更新的话，只需要在终端执行 sh update.sh 即可。
