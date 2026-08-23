f6916f5d4d581862d5d019597e57b8d3622ce9ec
花小孩 <14057717+flower-child@user.noreply.gitee.com>
2026-08-23 23:49:34 +0800

    开启 Waline 评论

---
+++ shims.d.ts
@@ -0,0 +1,17 @@
+// import 'valaxy'
+
+// declare module 'valaxy' {
+//   /**
+//    * extend post
+//    */
+//   interface Post {
+//     test: string
+//   }
+// }
+
+declare interface Window {
+  __DEV__: boolean
+  $pageData: any
+}
+
+declare module 'valaxy-addon-*'
